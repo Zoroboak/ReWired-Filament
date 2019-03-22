c0479b7bf02f5e49a17c8e51df1d58698be975fb
sanchezegido <pablosze@gmail.com>
2019-03-22 20:51:38 +0100

    Loaded an initial marker on the map

--- app/src/main/java/com/sazerotwo/rewiredfilament/MapActivity.kt
+++ app/src/main/java/com/sazerotwo/rewiredfilament/MapActivity.kt
@@ -1,24 +1,37 @@
 package com.sazerotwo.rewiredfilament
 
-import android.content.DialogInterface
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 
 class MapActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
+    private lateinit var map: MapboxMap
     private val LOCATION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, "pk.eyJ1Ijoic2FuY2hlemVnaWRvIiwiYSI6ImNqdGthcTIzajA5aWE0YXEzaXE0aWxteDgifQ.kSRTDPSqTZv3jGahAnF66w")
+        Mapbox.getInstance(
+            this,
+            "pk.eyJ1Ijoic2FuY2hlemVnaWRvIiwiYSI6ImNqdGthcTIzajA5aWE0YXEzaXE0aWxteDgifQ.kSRTDPSqTZv3jGahAnF66w"
+        )
         setContentView(R.layout.activity_map)
 
         mapView = findViewById(R.id.mapView)
@@ -37,29 +50,42 @@ class MapActivity : AppCompatActivity() {
         mapView.onResume()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
     public override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
 
     public override fun onStop() {
-        super.onStop()
         mapView.onStop()
+        super.onStop()
     }
 
     override fun onLowMemory() {
-        super.onLowMemory()
         mapView.onLowMemory()
+        super.onLowMemory()
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         mapView.onDestroy()
+        super.onDestroy()
     }
 
     private fun checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
         } else {
             loadMap()
         }
@@ -78,16 +104,62 @@ class MapActivity : AppCompatActivity() {
     private fun loadMap() {
         mapView.getMapAsync { mapboxMap ->
             mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                map = mapboxMap
+                enableLocationComponent()
+                loadMarkers()
             }
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent() {
+        // Get an instance of the component
+        val locationComponent = map.locationComponent
+
+        // Activate with options
+        locationComponent.activateLocationComponent(this, map.style!!)
+
+        // Enable to make component visible
+        locationComponent.isLocationComponentEnabled = true
+
+        // Set the component's camera mode
+        locationComponent.cameraMode = CameraMode.TRACKING
+
+        // Set the component's render mode
+        locationComponent.renderMode = RenderMode.COMPASS
+
+        locationComponent.isLocationComponentEnabled = true
+    }
+
+    private fun loadMarkers() {
+        map.style?.addImage(
+            "marker-icon-id",
+            BitmapFactory.decodeResource(
+                resources, R.drawable.mapbox_marker_icon_default
+            )
+        )
+
+        val geoJsonSource = GeoJsonSource(
+            "source-id", Feature.fromGeometry(
+                Point.fromLngLat(-5.663812, 40.965075)
+            )
+        )
+        map.style?.addSource(geoJsonSource)
+
+        val symbolLayer = SymbolLayer("layer-id", "source-id")
+        symbolLayer.withProperties(
+            PropertyFactory.iconImage("marker-icon-id"),
+            PropertyFactory.textField("hola")
+        )
+        map.style?.addLayer(symbolLayer)
     }
 
     private fun showAlert(msg: String) {
         val alertBuilder = AlertDialog.Builder(this)
             .setTitle("OJO")
             .setMessage(msg)
-            .setPositiveButton(android.R.string.ok
+            .setPositiveButton(
+                android.R.string.ok
             ) { dialog, _ -> dialog?.dismiss() }
 
         alertBuilder.create().show()
